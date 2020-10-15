e19d6510ff1cb1e0abed12821798c7f073be10b5
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-10-15 16:19:09 +0800

    rename brand name

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -35,7 +35,7 @@ const Navbar = ({toggle}) => {
         <>
             <Nav scrollNav={scrollNav}>
                 <NavbarContainer>
-                    <NavLogo to="/" onClick={toggleHome}>dolla</NavLogo>
+                    <NavLogo to="/" onClick={toggleHome}>vbank</NavLogo>
                     <MobileIcon onClick={toggle}>
                         <FaBars />
                     </MobileIcon>
